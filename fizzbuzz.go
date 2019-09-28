ee9c1481873418479388f4f17b3886ca36c70a4b
Morgan Murrah <morgan@easyrxcloud.com>
2019-09-28 13:48:06 -0700

    Identifying the 3 & 5 case

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -10,7 +10,13 @@ import "fmt"
 func main() {
 
 	for i := 0; i < 100; i++ {
-		fmt.Println(i)
+
+		// Start with most complicated first
+		// Multiple of 3 & 5
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		}
+
 	}
 
 }
