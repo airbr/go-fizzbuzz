45dd522ecdf44deeece97ab2ae4c7bbb788a94c6
Morgan Murrah <morgan@easyrxcloud.com>
2019-09-29 14:46:36 -0700

    Change format

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,22 +2,25 @@ package main
 
 import "fmt"
 
-//Fizz Buzz - Write a program that prints the numbers from 1 to 100.
-//But for multiples of three print “Fizz” instead of the number and
-//for the multiples of five print “Buzz”.
-//For numbers which are multiples of both three and five print “FizzBuzz”.
+/*
+The original classic overused initial job interview question
+
+Fizz Buzz - Write a program that prints the numbers from 1 to 100.
+for multiples of three print “Fizz” instead of the numver and
+for the multiples of five print “Buzz”
+for numbers which are multiples of both three and five print “FizzBuzz”.
+*/
 
 func main() {
 
 	for i := 0; i < 100; i++ {
 
-		// Start with most complicated first
 		if i%3 == 0 && i%5 == 0 {
 			fmt.Println("FizzBuzz")
-		} else if i%5 == 0 {
-			fmt.Println("Buzz")
 		} else if i%3 == 0 {
 			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
 		} else {
 			fmt.Println(i)
 		}
